744db47109334b06ed750170fc637c79524974e9
Jani Koivula <jkf.koivula@gmail.com>
2019-04-24 13:00:49 +0300

    Lukee databasesta ehdokkaidenn tiedot jsonniin

--- vaalikones/src/main/java/rest/HelloRestWorld.java
+++ vaalikones/src/main/java/rest/HelloRestWorld.java
@@ -9,19 +9,48 @@ import javax.ws.rs.Produces;
 
 import java.util.ArrayList;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Persistence;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.core.MediaType;
+import javax.persistence.Query;
 
+//import data.Riista;
+import persist.Ehdokkaat;
+import persist.Kysymykset;
 
-@Path("/riistaservice")
+@Path("/vaalikone")
 public class HelloRestWorld {
 
 	@GET
+	@Path("/terve")
 	@Produces(MediaType.TEXT_PLAIN)
-	public String terve() {
-		String output = "Terve!";
+	public String tervehdi() {
+		String output = "Terve, Pena!";
 		return output;
 	}
 
+	@GET
+	@Path("/tuoEhdokkaat")
+	@Produces(MediaType.APPLICATION_JSON)
+	public ArrayList<Ehdokkaat> getKaikki() {
 
+		EntityManagerFactory emf = null;
+		EntityManager em = null;
+		try {
+			emf = Persistence.createEntityManagerFactory("vaalikones");
+			em = emf.createEntityManager();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		ArrayList<Ehdokkaat> list = new ArrayList<>();
+		Query query = em.createNamedQuery("Ehdokkaat.findAll");
+
+		list.addAll(query.getResultList());
+		return list;
+	}
 }
